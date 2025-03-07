c48c1c888c71db70ac5b52f2f2c92c814c298fe0
AryanSaini1303 <saini.aryan9999@gmail.com>
2025-03-07 21:02:12 +0530

    Style: Refactor layout and styles for the login page, enhancing structure and visual presentation

--- app/page.js
+++ app/page.js
@@ -14,30 +14,35 @@ export default function Home() {
   console.log(session);
   return (
     <div className="wrapper">
-      <div className={styles.content}>
-        <header className={styles.header}>
-          <h2>Welcome to</h2>
-          <h1>Bharat Yatra Pass</h1>
-          <h5>Seamless ticket booking for India&apos;s iconic monuments &ndash; Fast, Secure, and Hassle-Free!</h5>
-        </header>
-        <button
-          onClick={() => signIn("google")}
-          className={`${styles.googleLogin} ${notoSans.className}`}
-        >
-          <Image
-            src={"/images/googleLogo.png"}
-            width={30}
-            height={30}
-            alt="Google Logo"
-          />
-          <h3> Sign In With Google</h3>
-        </button>
+      <div className={styles.loginPage}>
+        <div className={styles.content}>
+          <header className={styles.header}>
+            <h2>Welcome to</h2>
+            <h1>Bharat Yatra Pass</h1>
+            <h5>
+              Seamless ticket booking for India&apos;s iconic monuments &ndash;
+              Fast, Secure, and Hassle-Free!
+            </h5>
+          </header>
+          <button
+            onClick={() => signIn("google")}
+            className={`${styles.googleLogin} ${notoSans.className}`}
+          >
+            <Image
+              src={"/images/googleLogo.png"}
+              width={30}
+              height={30}
+              alt="Google Logo"
+            />
+            <h3> Sign In With Google</h3>
+          </button>
+        </div>
+        <img
+          src="/images/background-mobile.png"
+          alt=""
+          className={styles.background}
+        />
       </div>
-      <img
-        src="/images/background-mobile.png"
-        alt=""
-        className={styles.background}
-      />
     </div>
   );
 }
