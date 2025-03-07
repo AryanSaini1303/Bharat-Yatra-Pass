4504489b064194d033ae55bff76ffe11a7dad2e5
AryanSaini1303 <saini.aryan9999@gmail.com>
2025-03-07 20:32:42 +0530

    Feat: Added authentication with Google and update UI components completing the login page

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,18 @@
 import "./globals.css";
+import SessionWrapper from "@/components/SessionWrapper";
 
 export const metadata = {
   title: "Bharat Yatra Pass",
-  description: "BharatYatraPass – Book tickets effortlessly for India's iconic monuments with a secure and seamless experience. Digital tickets, QR verification, and hassle-free travel, all in one place!",
+  description:
+    "BharatYatraPass – Book tickets effortlessly for India's iconic monuments with a secure and seamless experience. Digital tickets, QR verification, and hassle-free travel, all in one place!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SessionWrapper>{children}</SessionWrapper>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,43 @@
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Noto_Sans } from "next/font/google";
+import { signIn, useSession } from "next-auth/react";
+
+const notoSans = Noto_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="wrapper">
-      <h1>Bharat Yatra Pass</h1>
+      <div className={styles.content}>
+        <header className={styles.header}>
+          <h2>Welcome to</h2>
+          <h1>Bharat Yatra Pass</h1>
+          <h5>Seamless ticket booking for India&apos;s iconic monuments &ndash; Fast, Secure, and Hassle-Free!</h5>
+        </header>
+        <button
+          onClick={() => signIn("google")}
+          className={`${styles.googleLogin} ${notoSans.className}`}
+        >
+          <Image
+            src={"/images/googleLogo.png"}
+            width={30}
+            height={30}
+            alt="Google Logo"
+          />
+          <h3> Sign In With Google</h3>
+        </button>
+      </div>
+      <img
+        src="/images/background-mobile.png"
+        alt=""
+        className={styles.background}
+      />
     </div>
   );
 }
