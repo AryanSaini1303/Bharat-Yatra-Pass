0fc6df1728e575674a70a2f5e95dabdd2f694902
AryanSaini1303 <saini.aryan9999@gmail.com>
2025-03-08 11:02:14 +0530

    Style: added cross logo when profile button is clicked

--- app/home/page.js
+++ app/home/page.js
@@ -20,13 +20,30 @@ export default function Home() {
           </section>
         </section>
         <section className={styles.profile}>
-          <img
-            src="/images/profile.png"
-            alt=""
-            onClick={() => {
-              setProfileClick(!profileClick);
-            }}
-          />
+          {!profileClick ? (
+            <img
+              src="/images/profile.png"
+              alt=""
+              onClick={() => {
+                setProfileClick(!profileClick);
+              }}
+            />
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="2.5em"
+              height="2.5em"
+              onClick={() => {
+                setProfileClick(!profileClick);
+              }}
+            >
+              <path
+                fill="currentColor"
+                d="M19.1 4.9C15.2 1 8.8 1 4.9 4.9S1 15.2 4.9 19.1s10.2 3.9 14.1 0s4-10.3.1-14.2m-4.3 11.3L12 13.4l-2.8 2.8l-1.4-1.4l2.8-2.8l-2.8-2.8l1.4-1.4l2.8 2.8l2.8-2.8l1.4 1.4l-2.8 2.8l2.8 2.8z"
+              ></path>
+            </svg>
+          )}
           {profileClick && (
             <ul className={styles.dropdown}>
               <li>
@@ -36,7 +53,13 @@ export default function Home() {
                 <button>Past Tickets</button>
               </li>
               <li>
-                <button onClick={(()=>{signOut({callbackUrl:"/"})})}>Log Out</button>
+                <button
+                  onClick={() => {
+                    signOut({ callbackUrl: "/" });
+                  }}
+                >
+                  Log Out
+                </button>
               </li>
             </ul>
           )}
