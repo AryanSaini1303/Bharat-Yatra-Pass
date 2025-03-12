604b9c6abaf17802bc96ee90f05498a9cba43ff9
AryanSaini1303 <saini.aryan9999@gmail.com>
2025-03-12 15:30:31 +0530

    save

--- app/booking/[...monumentId]/page.js
+++ app/booking/[...monumentId]/page.js
@@ -52,15 +52,6 @@ export default function BookingPage({ params }) {
         alert("Select at least 1 ticket before the checkout");
       }
     }
-    // console.log(dateTime.toLocaleTimeString());
-    // console.log(monument.opening_time);
-    if (
-      (dateTime.toLocaleTimeString() < monument.opening_time ||
-        dateTime.toLocaleTimeString() > monument.closing_time) &&
-      breakFlag
-    ) {
-      alert("Choose a suitable time slot!");
-    }
   }
 
   useEffect(() => {
