e3cecfa9306ba5b107e41611d7c5b63a64cb2563
Aryan Saini <saini.aryan9999@gmail.com>
2025-04-04 22:02:09 +0530

    save

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { Noto_Sans } from "next/font/google";
-import { useRouter } from "next/navigation";
 
 const notoSans = Noto_Sans({
   weight: "400",
@@ -12,41 +11,36 @@ const notoSans = Noto_Sans({
 });
 
 export default function Home() {
-  const [user, setUser]=useState('');
-  const router = useRouter();
+  const [user, setUser] = useState("");
   const [token, setToken] = useState(null);
 
   useEffect(() => {
-    if (router.isReady) {
-      const urlParams = new URLSearchParams(window.location.search);
-      const accessToken = urlParams.get('token');
+    const urlParams = new URLSearchParams(window.location.search);
+    const accessToken = urlParams.get("token");
 
-      if (accessToken) {
-        setToken(accessToken);
-        alert(accessToken);
-        // You can now use this token to authenticate with Supabase
-      } else {
-        console.warn('No token found in URL');
-      }
+    if (accessToken) {
+      setToken(accessToken);
+      console.log(accessToken);
+    } else {
+      console.warn("No token found in URL");
     }
-  }, [router.isReady]);
+  }, []);
 
   const signIn = async (redirectPath = "/home") => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
         redirectTo: `${window.location.origin}${redirectPath}`,
-        queryParams:{
-          prompt:"select_account" // Forces google to always show account selection
-        }
+        queryParams: {
+          prompt: "select_account", // Forces google to always show account selection
+        },
       },
     });
-  
+
     if (error) {
       console.error("Authentication Error:", error);
     }
   };
-  
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
@@ -65,16 +59,16 @@ export default function Home() {
     };
   }, []);
 
-  useEffect(()=>{
-    user&&window.location.replace("/home");
-  },[user])
+  useEffect(() => {
+    user && window.location.replace("/home");
+  }, [user]);
 
   return (
     <div className="wrapper">
       <div className={styles.loginPage}>
         <div className={styles.content}>
           <header className={styles.header}>
-          <p>{token}</p>
+            <p>{token}</p>
             <h2>Welcome to</h2>
             <h1>Bharat Yatra Pass</h1>
             <h5>
