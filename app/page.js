fa24e6de0aa91b60b197ed6a324d4d96527a4b0e
Aryan Saini <saini.aryan9999@gmail.com>
2025-04-09 13:05:59 +0530

    save

--- app/page.js
+++ app/page.js
@@ -4,11 +4,40 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { Noto_Sans } from "next/font/google";
+import axios from "axios";
 
 const notoSans = Noto_Sans({
   weight: "400",
   subsets: ["latin"],
 });
+
+const isValidSSOAccessToken = async (token) => {
+  const serverUrl = "https://keycloak.mogiio.com/";
+  const realmName = "udaipurinsider";
+  // console.log("realName : ", realmName, " ===> serverUrl : ", serverUrl);
+  const url = `${serverUrl}/realms/${realmName}/protocol/openid-connect/userinfo`;
+  try {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: "",
+    };
+    const response = await axios.request(config);
+    if (response.statusCode !== 200) {
+      console.log("Successfully Received the data from ", url);
+      return { status: true, data: response?.data };
+    } else {
+      return { status: false, message: "unable to validate keycloak" };
+    }
+  } catch (error) {
+    console.log("errors while keycloak validation ", error);
+    return { status: false, message: "unable to validate keycloak" };
+  }
+};
 
 export default function Home() {
   const [user, setUser] = useState("");
@@ -17,13 +46,25 @@ export default function Home() {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const accessToken = urlParams.get("token");
-    console.log(accessToken);
-    console.log(window.location.href);
+    // console.log(accessToken);
+    // console.log(window.location.href);
 
     if (accessToken) {
       setToken(accessToken);
-      console.log(accessToken);
-      console.log(urlParams);
+      // console.log(accessToken);
+      isValidSSOAccessToken(accessToken)
+        .then((response) => {
+          if (response.status) {
+            console.log("Token is valid");
+            // Perform any action you need with the valid token
+          } else {
+            console.error("Invalid token");
+            // Handle invalid token case
+          }
+        })
+        .catch((error) => {
+          console.error("Error validating token:", error);
+        });
     } else {
       console.warn("No token found in URL");
     }
